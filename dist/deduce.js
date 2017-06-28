84b1419b25417a1f594a3b87b0b1fa1a3c3d880b
Shannon Moeller <me@shannonmoeller.com>
2017-06-28 00:15:00 -0400

    Added parameter transform for Node 4.

--- dist/deduce.js
+++ dist/deduce.js
@@ -4,7 +4,10 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
-exports.default = function (state = {}) {
+exports.default = function () {
+	let state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+	let actions = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
 	const listeners = new Set();
 	let isDispatching = false;
 
@@ -37,14 +40,34 @@ exports.default = function (state = {}) {
 			throw new Error(`Action exists: ${name}`);
 		}
 
-		obj[name] = (...args) => {
+		obj[name] = function () {
+			for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+				args[_key] = arguments[_key];
+			}
+
 			dispatchAction(action, args);
 
 			return obj;
 		};
 	}
 
-	return {
+	function addActionFor(property, obj, name, action) {
+		if (typeof action !== 'function') {
+			throw new TypeError(`Action must be a function: ${name}`);
+		}
+
+		addAction(obj, name, function (state) {
+			for (var _len2 = arguments.length, args = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+				args[_key2 - 1] = arguments[_key2];
+			}
+
+			return Object.assign(state, {
+				[property]: action(state[property], ...args)
+			});
+		});
+	}
+
+	const store = {
 		get state() {
 			return state;
 		},
@@ -57,14 +80,28 @@ exports.default = function (state = {}) {
 			return this;
 		},
 
+		addActionsFor(property, actions) {
+			Object.keys(actions).forEach(name => {
+				addActionFor(property, this, name, actions[name]);
+			});
+
+			return this;
+		},
+
 		addListener(listener) {
+			if (typeof listener !== 'function') {
+				throw new TypeError(`Listener must be a function.`);
+			}
+
 			listeners.add(listener);
 
 			return () => {
-				listeners.remove(listener);
+				listeners.delete(listener);
 			};
 		}
 	};
+
+	return store.addActions(actions);
 };
 
 module.exports = exports['default'];
